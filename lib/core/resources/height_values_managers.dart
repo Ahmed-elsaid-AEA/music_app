959da3e2b1ceb03123e12e5d1c459760c32ca319
ahmed-abmouty <ahmed122727727@gmail.com>
2024-01-21 01:24:49 +0200

    design play music page part 3 (make custom songs details play music

--- lib/core/resources/height_values_managers.dart
+++ lib/core/resources/height_values_managers.dart
@@ -2,11 +2,13 @@ class HeightValuesManagers{
   static const double h1=1;
   static const double h4=4;
   static const double h9=9;
+  static const double h10=10;
   static const double h11=11;
   static const double h22=22;
   static const double h58=58;
   static const double h60=60;
   static const double h111=111;
+  static const double h124=124;
   static const double h150=150;
   static const double h171=171;
   static const double h199=199;
--- lib/features/play_music/screens/play_music_screen.dart
+++ lib/features/play_music/screens/play_music_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:music_apps/core/resources/alignment_managers.dart';
+import 'package:music_apps/core/resources/assets_managers.dart';
 import 'package:music_apps/core/resources/color_managers.dart';
+import 'package:music_apps/core/resources/font_managers.dart';
+import 'package:music_apps/core/resources/height_values_managers.dart';
+import 'package:music_apps/core/resources/radius_values_managers.dart';
 import 'package:music_apps/features/play_music/widgets/custom_app_bar_play_music_screen.dart';
+
+import '../widgets/custom_songs_details_play_music_screen.dart';
 
 class PlayMusicScreen extends StatelessWidget {
   const PlayMusicScreen({super.key});
@@ -9,19 +15,32 @@ class PlayMusicScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-extendBodyBehindAppBar: true,
-appBar: const CustomAppBarPlayMusicScreen(),
-      body: Container(  decoration: const BoxDecoration(
-
-          gradient: LinearGradient(
-              begin: Alignment(
-                  AlignmentManagersX.x0_20, AlignmentManagersY.y_0_98),
-              end: Alignment(
-                  AlignmentManagersX.x_0_20, AlignmentManagersY.y0_98),
-              colors: [
-                ColorManagers.kPrimaryColor,
-                ColorManagers.kDarkBlueColor
-              ])),),
+      extendBodyBehindAppBar: true,
+      appBar: const CustomAppBarPlayMusicScreen(),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment(
+                    AlignmentManagersX.x0_20, AlignmentManagersY.y_0_98),
+                end: Alignment(
+                    AlignmentManagersX.x_0_20, AlignmentManagersY.y0_98),
+                colors: [
+              ColorManagers.kPrimaryColor,
+              ColorManagers.kDarkBlueColor
+            ])),
+        child: SafeArea(
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: HeightValuesManagers.h150,),
+              CustomSongsDetailsPlayMusic()
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
